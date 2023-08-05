e485201a18cf8f60988b25266ddf1e1aa5a92724
YellowBicycleee <wangjianchengcs@163.com>
2023-08-05 22:03:53 +0800

    add file that can test eo pre

--- tests/test.dslash.qcu.py
+++ tests/test.dslash.qcu.py
@@ -1,11 +1,51 @@
 import os
 import sys
 import timeit
-
-# test_dir = os.path.dirname(os.path.abspath(__file__))
-# sys.path.insert(0, os.path.join(test_dir, ".."))
-
 import numpy as np
+
+
+from pyquda.field import lexico
+def eo_pre(data: np.ndarray, axes: List[int], dtype=None):
+    shape = data.shape
+    Lt, Lz, Ly, Lx = [shape[axis] for axis in axes]
+    Lx //= 2
+    Npre = int(np.prod(shape[:axes[0]]))
+    Nsuf = int(np.prod(shape[axes[-1] + 1:]))
+    dtype = data.dtype if dtype is None else dtype
+    # data_cb2 = data.reshape(Npre, 2, Lt, Lz, Ly, Lx // 2, Nsuf)
+    data_cb2 = data.reshape(Npre, Lt, Lz, Ly, Lx, Nsuf)
+    data_new = np.zeros((Npre, 2, Lt, Lz, Ly, Lx//2, Nsuf), dtype)
+    for t in range(Lt):
+        for z in range(Lz):
+            for y in range(Ly):
+                eo = (t + z + y) % 2
+                if eo == 0:
+                    data_new[:, 0, t, z, y, :] = data_cb2[:, t, z, y, 0::2]
+                    data_new[:, 1, t, z, y, :] = data_cb2[:, t, z, y, 1::2]
+                    # data_new[:, t, z, y, 0::2] = data_cb2[:, 0, t, z, y, :]
+                    # data_new[:, t, z, y, 1::2] = data_cb2[:, 1, t, z, y, :]
+                else:
+                    data_new[:, 1, t, z, y, :] = data_cb2[:, t, z, y, 0::2]
+                    data_new[:, 0, t, z, y, :] = data_cb2[:, t, z, y, 1::2]
+                    # data_new[:, t, z, y, 1::2] = data_cb2[:, 0, t, z, y, :]
+                    # data_new[:, t, z, y, 0::2] = data_cb2[:, 1, t, z, y, :]
+    return data_new.reshape(*shape[:axes[0]], 2, Lt, Lz, Ly, Lx//2, *shape[axes[-1] + 1:])
+
+def vector_lexico(vec): #(2, Lt, Lz, Ly, Lx, Nd, Nc)
+    result = lexico(vec, [0, 1, 2, 3, 4])
+    return result
+def gauge_lexico(vec): #(Nd, 2, Lt, Lz, Ly, Lx, Nc, Nc)
+    result = lexico(vec, [1, 2, 3, 4, 5])
+    return result
+def vector_eo_pre(vec):
+    result = eo_pre(vec, [0, 1, 2, 3])
+    return result
+def gauge_eo_pre(vec):
+    result = eo_pre(vec, [1, 2, 3, 4])
+    return result
+
+
+
 
 from pyquda import init, pyqcu
 
@@ -29,8 +69,7 @@ def applyDslash(Mp, p, U_seed):
 
     # Generate gauge and then load it
     U = gauge_utils.gaussGauge(latt_size, U_seed)
-    #dslash.loadGauge(U)
-    t1 = timeit.timeit(lambda: dslash.loadGauge(U), number=1)
+    dslash.loadGauge(U)
 
     # Load a from p and allocate b
     a = LatticeFermion(latt_size, cp.asarray(core.cb2(p, [0, 1, 2, 3])))
@@ -49,50 +88,49 @@ def applyDslash(Mp, p, U_seed):
     # Return gauge as a ndarray with shape (Nd, Lt, Lz, Ly, Lx, Ns, Ns)
     return U.lexico(), t2+t3
 
-
+Mp = np.zeros((Lt, Lz, Ly, Lx, Ns, Nc), np.complex128)
 p = np.zeros((Lt, Lz, Ly, Lx, Ns, Nc), np.complex128)
 p[0, 0, 0, 0, 0, 0] = 1
-p[0, 0, 0, 0, 0, 1] = 1
-p[0, 1, 0, 0, 0, 0] = 1
-Mp = np.zeros((Lt, Lz, Ly, Lx, Ns, Nc), np.complex128)
-
 U,_ = applyDslash(Mp, p, 0)
-print(Mp[0, 15, 0, 0])
 
 Mp1 = np.zeros((Lt, Lz, Ly, Lx, Ns, Nc), np.complex128)
 param = pyqcu.QcuParam()
 param.lattice_size = latt_size
-pyqcu.dslashQcu(Mp1, p, U, param)
-print(Mp1[0, 15, 0, 0])
-
+p_pre = vector_eo_pre(p)
+Mp_pre = vector_eo_pre(Mp)
+U_pre = gauge_eo_pre(U)
+pyqcu.dslashQcu(Mp1[0], p_pre[1], U, param, 0)
+pyqcu.dslashQcu(Mp1[1], p_pre[0], U, param, 1)
+my_res = vector_eo_pre(Mp)
+'----------------------------------------'
 
 print(np.linalg.norm(Mp - Mp1))
 #print(np.linalg.norm(Mp - Mp1)/np.linalg.norm(Mp))
 
-def compare(round):
-    print('===============round ', round, '======================')
-    shape = (Lt, Lz, Ly, Lx, Ns, Nc)
-    p = np.random.randn(*shape, 2).view(np.complex128).reshape(shape)
-    print(p.shape)
-    Mp = np.zeros((Lt, Lz, Ly, Lx, Ns, Nc), np.complex128)
-    U, time_quda = applyDslash(Mp, p, 0)
-    print('Quda dslash: = ', time_quda, 's')
-    if round == 0:
-        print('QUDA result of [0, 0, 0, 1]')
-        print(Mp[0, 0, 0, 1])
-    Mp1 = np.zeros((Lt, Lz, Ly, Lx, Ns, Nc), np.complex128)
-    param = pyqcu.QcuParam()
-    param.lattice_size = latt_size
-    t = timeit.timeit(lambda: pyqcu.dslashQcu(Mp1, p, U, param), number=1)
-    print('my_dslash: = ', t, 's')
-    if round == 0:
-        print('My result of [0, 0, 0, 1]')
-        print(Mp1[0, 0, 0, 1])
-    print('difference: ', np.linalg.norm(Mp-Mp1)/np.linalg.norm(Mp))
+# def compare(round):
+#     print('===============round ', round, '======================')
+#     shape = (Lt, Lz, Ly, Lx, Ns, Nc)
+#     p = np.random.randn(*shape, 2).view(np.complex128).reshape(shape)
+#     print(p.shape)
+#     Mp = np.zeros((Lt, Lz, Ly, Lx, Ns, Nc), np.complex128)
+#     U, time_quda = applyDslash(Mp, p, 0)
+#     print('Quda dslash: = ', time_quda, 's')
+#     if round == 0:
+#         print('QUDA result of [0, 0, 0, 1]')
+#         print(Mp[0, 0, 0, 1])
+#     Mp1 = np.zeros((Lt, Lz, Ly, Lx, Ns, Nc), np.complex128)
+#     param = pyqcu.QcuParam()
+#     param.lattice_size = latt_size
+#     t = timeit.timeit(lambda: pyqcu.dslashQcu(Mp1, p, U, param), number=1)
+#     print('my_dslash: = ', t, 's')
+#     if round == 0:
+#         print('My result of [0, 0, 0, 1]')
+#         print(Mp1[0, 0, 0, 1])
+#     print('difference: ', np.linalg.norm(Mp-Mp1)/np.linalg.norm(Mp))
 
-# from time import perf_counter
+# # from time import perf_counter
 
 
 
-for i in range(0, 5):
-    compare(i)
+# for i in range(0, 5):
+#     compare(i)
